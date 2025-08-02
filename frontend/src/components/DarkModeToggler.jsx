3a6720137b692d4cc4079891604d5a5ac181ebd5
Muhammed Arshaq <arshaq31@gmail.com>
2025-08-03 03:15:22 +0530

    add: theme toggle using context

---
+++ frontend/src/components/DarkModeToggler.jsx
@@ -0,0 +1,17 @@
+import { useDarkMode } from "../context/ThemeContext";
+import { Moon, SunMedium } from "lucide-react";
+import React from "react";
+
+const DarkModeToggler = () => {
+  const { darkMode, setDarkMode } = useDarkMode();
+  return (
+    <button
+      onClick={() => setDarkMode(!darkMode)}
+      className="bg-gray-200 dark:bg-gray-800 rounded-full p-3 cursor-pointer hover:text-blue-300 dark:hover:text-yellow-600"
+    >
+      {darkMode ? <SunMedium size={18} /> : <Moon size={18} />}
+    </button>
+  );
+};
+
+export default DarkModeToggler;
